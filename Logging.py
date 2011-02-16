1736d8192a4649f71cccd113fb92ab081f55948e
Richard Schreiber <wiffbi@gmail.com>
2011-02-16 11:29:15 +0100

    added utility function to print numbers as binary - useful for MIDI debug

--- Logging.py
+++ Logging.py
@@ -6,3 +6,9 @@ def log(msg):
 	f = open(__file__.replace('.pyc', '.py').replace('Logging.py','SelectedTrackControl.log'), 'a')
 	f.write(msg+"\n")
 	f.close()
+
+
+def bin(x, digits=0): 
+	oct2bin = ['000','001','010','011','100','101','110','111'] 
+	binstring = [oct2bin[int(n)] for n in oct(x)] 
+	return ''.join(binstring).lstrip('0').zfill(digits)
