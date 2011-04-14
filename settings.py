f7e9f52e67495daea70265421d0b0107354f5aed
Richard Schreiber <wiffbi@gmail.com>
2011-04-14 18:25:07 +0200

    activated solo control again

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ midi_mapping = {
 	"arm": Note(0),
 	"arm_exclusive": Note(3),
 	"arm_kill": Note(10),
-	#"solo": Note(1),
+	"solo": Note(1),
 	"solo_exclusive": Note(4),
 	"solo_kill": Note(7),
 	"mute": Note(2),
