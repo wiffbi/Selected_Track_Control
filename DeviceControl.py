63fdf40eb4d738bdb96838f94503c9890306af56
Richard Schreiber <wiffbi@gmail.com>
2011-08-11 10:20:07 +0200

    Fixed Device-Control with relative CC-values. As parameter values can vary in range quite a bit, this adds some logic to guess what is the best increment/decrement on the current value.

--- DeviceControl.py
+++ DeviceControl.py
@@ -129,19 +129,31 @@ class DeviceControl(Control):
 		
 		param_range = param.max - param.min
 		if param.is_quantized:
-			if mode == MIDI.ABSOLUTE:
-				if status == MIDI.NOTEON_STATUS:
-					# if Note On => toggle quantized value
-					param.value = not param.value
-				else:
-					param.value = round(param_range*value/127.0 + param.min)
+			if status == MIDI.CC_STATUS and mode == MIDI.ABSOLUTE:
+				# absolute CC
+				param.value = round(param_range*value/127.0 + param.min)
 			else:
-				param.value = max(param.min, min(param.max, param.value + value))
+				# relative CC or NOTE
+				if param_range == 1 or status == MIDI.NOTEON_STATUS:
+					# circle through quantized values
+					p_value = param.value + value
+					if p_value > param.max:
+						p_value = param.min
+					param.value = p_value
+				else:
+					# range is bigger than on/off and we have relative CC
+					# => do NOT circle through quantized values
+					param.value = max(param.min, min(param.max, param.value + value))
 		else:
 			if mode == MIDI.ABSOLUTE:
 				param.value = param_range*value/127.0 + param.min
 			else:
-				param.value = max(param.min, min(param.max, param.value + (value/100.0)))
+				#param.value = max(param.min, min(param.max, param.value + (value/100.0)))
+				if param_range > 4:
+					param.value = max(param.min, min(param.max, param.value + value))
+				else:
+					param.value = max(param.min, min(param.max, param.value + param_range*value/127.0))
+				
 	
 	
 	
